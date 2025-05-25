9e2ef4dacbc9c45a6a61e0c40fe10de4ec4127bb
Harsh Dalmia <harshdalmia20212081@gmail.com>
2025-05-25 21:05:37 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const http=require('http');
+const express=require('express');
+const mongoose=require('mongoose')
+const {Server}=require('socket.io')
+const cors = require('cors');
+require('dotenv').config();
+
+const app=express();
+const server=http.createServer(app);
+const io=new Server(server,{
+    cors:{
+        origin:'*',
+    }
+});
+app.use(cors());
+
+
+app.get('/',(req,res)=>{
+    res.send("Server is Running")
+});
+
+server.listen(process.env.PORT,(req,res)=>{
+    console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`)
+})
+
